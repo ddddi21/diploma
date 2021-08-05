9e3d8f9a8c49922bb9186eb4f84abbe32bffe950
Dilyara Askhadullina <ashadullina@technokratos.com>
2021-08-05 16:07:39 +0300

    set up bottom navigation visibility

--- app/src/main/java/com/technokratos/app/presentation/MainActivity.kt
+++ app/src/main/java/com/technokratos/app/presentation/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.technokratos.app.presentation
 
+import android.view.View
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
@@ -9,6 +10,7 @@ import com.technokratos.app.R
 import com.technokratos.app.di.deps.findComponentDependencies
 import com.technokratos.app.di.main.MainComponent
 import com.technokratos.common.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_main.bottomNavigationView
 import javax.inject.Inject
 
 class MainActivity : BaseActivity<MainViewModel>() {
@@ -27,15 +29,10 @@ class MainActivity : BaseActivity<MainViewModel>() {
     }
 
     override fun initViews() {
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.main_nav_host_fragment) as NavHostFragment? ?: return
-        navController = navHostFragment.navController
-        navigator.attachNavController(navController, R.navigation.main_nav_graph)
-        val bottomNavView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
-        NavigationUI.setupWithNavController(bottomNavView, navController)
+        setUpNavigation()
         viewModel.onSplashAnimationFinished()
     }
-    // вынесу мейн штуки в отдельный модуль в другом пр и там же буду настраивать visibility y bottomNav
+    // вынесу мейн штуки в отдельный модуль в другом пр
 
     override fun subscribe(viewModel: MainViewModel) {
         // TODO
@@ -44,5 +41,30 @@ class MainActivity : BaseActivity<MainViewModel>() {
     override fun onDestroy() {
         super.onDestroy()
         navigator.detachNavController(navController)
+    }
+
+    private fun setUpBottomNavVisibility() {
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.splashFragment,
+                R.id.authFragment,
+                R.id.registrationFragment -> {
+                    bottomNavigationView.visibility = View.GONE
+                }
+                else ->
+                    bottomNavigationView.visibility = View.VISIBLE
+            }
+        }
+    }
+
+    private fun setUpNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.main_nav_host_fragment) as NavHostFragment?
+                ?: return
+        navController = navHostFragment.navController
+        navigator.attachNavController(navController, R.navigation.main_nav_graph)
+        val bottomNavView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        NavigationUI.setupWithNavController(bottomNavView, navController)
+        setUpBottomNavVisibility()
     }
 }
