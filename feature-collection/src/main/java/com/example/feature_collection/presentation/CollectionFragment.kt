bc255d7aee9273b9c403f664ee3d5c1fdeba915a
Dilyara Askhadullina <ashadullina@technokratos.com>
2021-08-09 14:13:23 +0300

    rename fragment

--- feature-collection/src/main/java/com/example/feature_collection/presentation/CollectionFragment.kt
+++ feature-collection/src/main/java/com/example/feature_collection/presentation/CollectionFragment.kt
@@ -11,7 +11,7 @@ import com.example.feature_collection.R
 import com.example.feature_collection.databinding.CollectionFragmentBinding
 import com.example.feature_collection.di.CollectionFeatureComponent
 import com.example.feature_collection.di.CollectionFeatureKey
-import com.example.feature_collection.presentation.filmCollection.CollectionFilmListFragment
+import com.example.feature_collection.presentation.filmCollection.WatchedCollectionFilmsFragment
 import com.example.feature_collection.presentation.watchLater.WillWatchLaterListFragment
 import com.google.android.material.tabs.TabLayoutMediator
 import com.technokratos.common.base.BaseFragment
@@ -64,7 +64,7 @@ class DemoCollectionAdapter(fragment: Fragment) : FragmentStateAdapter(fragment)
 
     override fun createFragment(position: Int): Fragment {
         return when (position) {
-            0 -> CollectionFilmListFragment()
+            0 -> WatchedCollectionFilmsFragment()
             else -> WillWatchLaterListFragment()
         }
     }
--- feature-collection/src/main/java/com/example/feature_collection/presentation/filmCollection/WatchedCollectionFilmsFragment.kt
+++ feature-collection/src/main/java/com/example/feature_collection/presentation/filmCollection/WatchedCollectionFilmsFragment.kt
@@ -5,14 +5,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.feature_collection.databinding.FilmCollectionListFragmentBinding
+import com.example.feature_collection.databinding.WatchedFilmCollectionFragmentBinding
 
-class CollectionFilmListFragment : Fragment() {
+class WatchedCollectionFilmsFragment : Fragment() {
 
-    private lateinit var binding: FilmCollectionListFragmentBinding
+    private lateinit var binding: WatchedFilmCollectionFragmentBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        binding = FilmCollectionListFragmentBinding.inflate(layoutInflater)
+        binding = WatchedFilmCollectionFragmentBinding.inflate(layoutInflater)
         return binding.root
     }
 }
