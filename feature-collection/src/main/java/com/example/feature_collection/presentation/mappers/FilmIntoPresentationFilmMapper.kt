4579808f06f676a8ff1909ae1ab1db134a19a93b
Dilyara Askhadullina <ashadullina@technokratos.com>
2021-08-26 12:10:15 +0300

    fix remarks

--- feature-collection/src/main/java/com/example/feature_collection/presentation/mappers/FilmIntoPresentationFilmMapper.kt
+++ feature-collection/src/main/java/com/example/feature_collection/presentation/mappers/FilmIntoPresentationFilmMapper.kt
@@ -2,13 +2,14 @@ package com.example.feature_collection.presentation.mappers
 
 import com.example.feature_collection.data.network.model.Film
 import com.example.feature_collection.model.FilmGridItem
+import com.example.feature_collection.model.FilmLinearItem
 import javax.inject.Inject
 
 private const val DEFAULT_POSTER_URL = "https://upload.wikimedia.org/wikipedia/ru/thumb/6/6e/%D0%9E%D1%81%D1%82%D1%80%D0%9A%D0%BE%D0%B7.jpg/274px-%D0%9E%D1%81%D1%82%D1%80%D0%9A%D0%BE%D0%B7.jpg"
 
-class FilmIntoGridFilmMapper @Inject constructor() {
+class FilmIntoPresentationFilmMapper @Inject constructor() {
 
-    fun map(model: Film, onItemClicked: ((Int) -> Unit)? = null): FilmGridItem {
+    fun mapIntoGridFilm(model: Film, onItemClicked: ((Int) -> Unit)? = null): FilmGridItem {
         return FilmGridItem(
             id = model.id,
             title = model.title,
@@ -16,6 +17,14 @@ class FilmIntoGridFilmMapper @Inject constructor() {
             genres = model.genres ?: listOf(""),
             rating = model.rating ?: 0.0,
             posterUrl = model.posterUrl ?: DEFAULT_POSTER_URL,
+            onItemClicked = onItemClicked
+        )
+    }
+
+    fun mapIntoLinearFilm(model: Film, onItemClicked: ((Int) -> Unit)? = null): FilmLinearItem {
+        return FilmLinearItem(
+            id = model.id,
+            title = model.title,
             onItemClicked = onItemClicked
         )
     }
